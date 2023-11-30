d43df881da1437787254390526b4a37e019337dd
geffaa <yodhimas02@gmail.com>
2023-11-30 10:01:35 +0700

    add update

--- app/src/main/java/com/example/ppapb_pertemuan14_notification/MainActivity.kt
+++ app/src/main/java/com/example/ppapb_pertemuan14_notification/MainActivity.kt
@@ -5,6 +5,7 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -14,7 +15,6 @@ import com.example.ppapb_pertemuan14_notification.databinding.ActivityMainBindin
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val channelId = "TEST_NOTIF"
-    private val notifId = 90
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,36 +24,69 @@ class MainActivity : AppCompatActivity() {
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE)
                 as NotificationManager
 
+        binding.btnUpdate.setOnClickListener {
+            val notifImage = BitmapFactory.decodeResource(resources, R.drawable.yodhim)
+            val builder = NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.drawable.baseline_notifications_24)
+                .setContentTitle("Notifikasi Saya")
+                .setContentText("Hello World")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(true)
+                .setLargeIcon(notifImage)
+                .setStyle(
+                    NotificationCompat.BigPictureStyle()
+                        .bigPicture(notifImage)
+                        .bigLargeIcon(null)
+                )
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val channel = NotificationChannel(
+                    channelId,
+                    "Notifku",
+                    NotificationManager.IMPORTANCE_HIGH
+                )
+                with(notificationManager) {
+                    createNotificationChannel(channel)
+                    notify(0, builder.build())
+                }
+            } else {
+                notificationManager.notify(0, builder.build())
+            }
+        }
+
         binding.btnNotif.setOnClickListener{
-
-
             val flag = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 PendingIntent.FLAG_MUTABLE
             } else {
                 0
             }
 
-            val intent = Intent(this, NotifReceiver::class.java).putExtra("MESSAGE", "Baca Selengkapnya")
+            val intent = Intent(this, NotifReceiver::class.java).putExtra("Message", "Baca Selengkapnya")
             val pendingIntent = PendingIntent.getBroadcast(this, 0, intent, flag)
 
             val builder = NotificationCompat.Builder(this, channelId)
                 .setSmallIcon(R.drawable.baseline_notifications_24)
-                .setContentTitle("My Notification")
-                .setContentText("hello world!")
+                .setContentTitle("Notifikasi Saya")
+                .setContentText("Hello World")
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setAutoCancel(true)
-                .addAction(0, "Baca Notif", pendingIntent)
+                .addAction(0, "Baca Selengkapnya", pendingIntent)
+
+
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                val notifChannel = NotificationChannel(channelId, "Notifku", NotificationManager.IMPORTANCE_DEFAULT)
-                with(notificationManager){
-                    createNotificationChannel(notifChannel)
+                val channel = NotificationChannel(
+                    channelId,
+                    "Notifku",
+                    NotificationManager.IMPORTANCE_HIGH
+                )
+                with(notificationManager) {
+                    createNotificationChannel(channel)
                     notify(0, builder.build())
                 }
             } else {
                 notificationManager.notify(0, builder.build())
             }
-
         }
     }
 }
--- app/src/main/java/com/example/ppapb_pertemuan14_notification/NotifReceiver.kt
+++ app/src/main/java/com/example/ppapb_pertemuan14_notification/NotifReceiver.kt
@@ -6,12 +6,16 @@ import android.content.Intent
 import android.widget.Toast
 
 class NotifReceiver : BroadcastReceiver() {
-
     override fun onReceive(context: Context, intent: Intent) {
-        val msg = intent.getStringExtra("MESSAGE")
-        if(msg != null) {
+        val msg = intent.getStringExtra("Message")
+        if (msg != null) {
             Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
         }
+
+        // Start MainActivity after showing the Toast
+        val activityIntent = Intent(context, MainActivity::class.java)
+        activityIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        context.startActivity(activityIntent)
     }
 }
 
