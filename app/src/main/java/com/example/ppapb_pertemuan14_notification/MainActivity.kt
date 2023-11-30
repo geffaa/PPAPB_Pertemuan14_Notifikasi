bd95e35eba291911dc386dacc379c8491df72883
geffaa <yodhimas02@gmail.com>
2023-11-30 09:13:29 +0700

    first commit

---
+++ app/src/main/java/com/example/ppapb_pertemuan14_notification/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.ppapb_pertemuan14_notification
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import com.example.ppapb_pertemuan14_notification.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val channelId = "TEST_NOTIF"
+    private val notifId = 90
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE)
+                as NotificationManager
+
+        binding.btnNotif.setOnClickListener{
+
+            val intent = Intent(this, MainActivity::class.java)
+            val flag = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                PendingIntent.FLAG_MUTABLE
+            } else {
+                0
+            }
+
+            val pendingIntent = PendingIntent.getActivity(this, 0, intent, flag)
+
+            val builder = NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.drawable.baseline_notifications_24)
+                .setContentTitle("My Notification")
+                .setContentText("hello world!")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val notifChannel = NotificationChannel(channelId, "Notifku", NotificationManager.IMPORTANCE_DEFAULT)
+                with(notificationManager){
+                    createNotificationChannel(notifChannel)
+                    notify(0, builder.build())
+                }
+            } else {
+                notificationManager.notify(0, builder.build())
+            }
+
+        }
+    }
+}
